dcaaddfe29add4806942e5f1f6607d891c026dd7
Jorge Longoria <jlongo48@stu.southtexascollege.edu>
2022-10-15 19:33:51 -0500

    Fetched characters

---
+++ src/composables/useCharacters.js
@@ -0,0 +1,24 @@
+import { ref } from 'vue'
+import useApi from '@/composables/useApi'
+
+const characters = ref([])
+const api = useApi()
+const page = ref(1)
+
+const useCharacters = () => {
+  const fetchCharacters = async () => {
+    const { data } = await api.instance.get('/characters', {
+      params: {
+        page: page.value,
+      },
+    })
+    characters.value.push(...data.data)
+    page.value++
+  }
+
+  return {
+    characters,
+    fetchCharacters}
+}
+
+export default useCharacters
